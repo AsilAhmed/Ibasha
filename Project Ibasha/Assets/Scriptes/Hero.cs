6504f5f1e7cccdb01b954fda860535c182839009
AsilAhmed <asilahmedipad@gmail.com>
2022-06-15 20:43:22 +0500

    Add project Ibasha

---
+++ Project Ibasha/Assets/Scriptes/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jump_speed = 8f;
+    public float timedelay = 0.5f;
+    public float Radius;
+    public float movement;
+    Rigidbody2D rb;
+    public Transform groundcheckpoint;
+    public LayerMask groundlayer;
+    bool isTouchingground;
+    public Animator anim;
+    public Vector3 respawn;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        respawn = transform.position;
+    }
+
+    void Update()
+    {
+        isTouchingground = Physics2D.OverlapCircle(groundcheckpoint.position, Radius, groundlayer);
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (movement < 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump") && isTouchingground)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_speed);
+        }
+    }
+}
