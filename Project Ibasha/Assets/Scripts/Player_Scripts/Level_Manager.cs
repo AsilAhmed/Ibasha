f64eb3def245aaa6caf3aef7687d75f0c9253737
mhamzabaig <mhamzabaig1234@gmail.com>
2022-06-28 20:27:06 +0500

    Some Sounds added

---
+++ Project Ibasha/Assets/Scripts/Player_Scripts/Level_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int TotalCash;
+    Text MoneyText;
+    void Start()
+    {
+        MoneyText = GameObject.FindGameObjectWithTag("Money").GetComponent<Text>();   
+    }
+
+    // Update is called once per frame
+    
+
+    public void Coins_Counter(int Score)
+    {
+        TotalCash += Score;
+        MoneyText.text = "Cash: " + TotalCash.ToString() + "$";
+        
+;    }
+}
